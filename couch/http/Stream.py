87a0e653ccd9ea493dbdac61800d3af0759a1cd2
Kerem Güneş <qeremy@gmail.com>
2016-01-30 19:57:34 +0200

    add http.Stream object.

--- couch/http/Stream.py
+++ couch/http/Stream.py
@@ -1,6 +1,44 @@
+import couch.util.Util as util
+
 class Stream(object):
    type = None
+   httpVersion = None
+   headers = {}
+   body = None
    def __init__(self, headers = {}, body = None):
+      self.headers = headers
+      self.body = body
+
+   def getData(self, key=None):
+      if key == None:
+         return self.body
+      return util.dig(key, self.body)
+
+   def setBody(self, body=None):
+      if self.__class__.__name__ == "Stream":
+         raise Exception("You should re-define [<OBJECT>].setBody(self, body=None) method!")
+
+   def getBody(self):
+      return self.body
+
+   def setHeader(self, key, value=None):
+      if value == None:
+         if key in self.headers:
+            del self.headers[key]
+      else:
+         self.headers[key] = value
+
+   def getHeader(self, key):
+      if key in self.headers:
+         return self.headers[key]
+
+   def getHeaderAll(self):
+      return self.headers
+
+   def toString(self):
       pass
 
-
+Stream.TYPE = {
+   "REQUEST": 1,
+   "RESPONSE": 2
+}
