96e1a530c3a5ed6411de23b3d46891ed31f00ba7
Kerem Güneş <qeremy@gmail.com>
2016-01-30 19:08:09 +0200

    add getObjectName().

--- couch/util/Util.py
+++ couch/util/Util.py
@@ -1,15 +1,18 @@
 import pprint
 
-def pre(x):
-   pprint.pprint(x)
+def pre(o):
+   pprint.pprint(o)
 
-def prd(x, all=False):
-   name = "%s" % (x)
-   name = name[1:name.find(" ")]
-   for attr in dir(x):
+def prd(o, all=False):
+   name = getObjectName(o)
+   for attr in dir(o):
       # show only attrs
       if all == False and attr[:2] != "__":
-         print "<%s>.%s = %s" % (name, attr, getattr(x, attr))
+         print "<%s>.%s = %s" % (name, attr, getattr(o, attr))
       elif all == True:
-         print "<%s>.%s = %s" % (name, attr, getattr(x, attr))
+         print "<%s>.%s = %s" % (name, attr, getattr(o, attr))
 
+def getObjectName(o):
+   name = "%s" % (o)
+   name = name[1:name.find(" ")]
+   return name
