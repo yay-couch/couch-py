fa93d71274781743360de3485b60b7edb27a415a
Kerem Güneş <qeremy@gmail.com>
2016-01-30 18:49:26 +0200

    update dumper func.

--- couch/util/Util.py
+++ couch/util/Util.py
@@ -3,6 +3,13 @@ import pprint
 def pre(x):
    pprint.pprint(x)
 
-def prd(obj):
-   for attr in dir(obj):
-      print ">> obj.%s = %s" % (attr, getattr(obj, attr))
+def prd(x, all=False):
+   name = "%s" % (x)
+   name = name[1:name.find(" ")]
+   for attr in dir(x):
+      # show only attrs
+      if all == False and attr[:2] != "__":
+         print "<%s>.%s = %s" % (name, attr, getattr(x, attr))
+      elif all == True:
+         print "<%s>.%s = %s" % (name, attr, getattr(x, attr))
+
