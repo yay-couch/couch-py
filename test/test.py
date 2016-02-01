55f28506e2abe1cca065d301e5b0c223b11ba9d6
Kerem Güneş <qeremy@gmail.com>
2016-02-01 03:24:22 +0200

    tests..

--- test/test.py
+++ test/test.py
@@ -2,7 +2,7 @@ import couch as _couch
 # import couch.util.Util as util
 from couch.util.Util import *
 
-couch = _couch.Couch({}, not True)
+couch = _couch.Couch({}, True)
 client = _couch.Client(couch)
 
 # client.get("/?a=1")
@@ -10,11 +10,15 @@ client = _couch.Client(couch)
 # print(client.getRequest().toString())
 # print(client.getResponse().toString())
 
-# response = client.get("/?a=1")
+response = client.get("/?a=1")
 # print response.getBody()
 # print response.getBodyData()
 # print response.getBodyData("uuid")
 # print response.getBodyData()["uuid"]
+# print response.getBodyData("vendor.version")
 
-# @todo
 server = _couch.Server(client)
+# print server.ping()
+# print server.info()
+# print server.info("vendor.name")
+# print server.version()
