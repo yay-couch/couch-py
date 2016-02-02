e3fc21d35c65c748d2e74304abcfe961f38a45d6
Kerem Güneş <qeremy@gmail.com>
2016-02-02 02:21:57 +0200

    add ping().

--- couch/Database.py
+++ couch/Database.py
@@ -4,3 +4,6 @@ class Database():
    def __init__(self, client, name):
       self.client = client
       self.name = name
+
+   def ping(self):
+      return (200 == self.client.head(self.name).getStatusCode())
