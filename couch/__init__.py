5e8cf1719d5cacc5c88da629b665f62924d89daa
Kerem Güneş <qeremy@gmail.com>
2016-02-06 19:54:56 +0200

    remove generateUuid import.

--- couch/__init__.py
+++ couch/__init__.py
@@ -3,5 +3,5 @@ from Client import Client
 from Server import Server
 from Database import Database
 from Document import Document
-from Uuid import Uuid, generate as generateUuid
+from Uuid import Uuid
 # from http.Stream import Stream
