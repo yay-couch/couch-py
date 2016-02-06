f06881b2c67ff431b731cbf43b8839d5331c40d8
Kerem Güneş <qeremy@gmail.com>
2016-02-06 19:46:19 +0200

    add generateUuid.

--- couch/__init__.py
+++ couch/__init__.py
@@ -3,5 +3,5 @@ from Client import Client
 from Server import Server
 from Database import Database
 from Document import Document
-from Uuid import Uuid
+from Uuid import Uuid, generate as generateUuid
 # from http.Stream import Stream
