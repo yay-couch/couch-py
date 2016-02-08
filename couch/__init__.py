53c65e70f6ceb3fbc528cfad571d64d5aa3f785a
Kerem Güneş <qeremy@gmail.com>
2016-02-08 02:54:20 +0200

    add DocumentAttachment.

--- couch/__init__.py
+++ couch/__init__.py
@@ -3,5 +3,6 @@ from Client import Client
 from Server import Server
 from Database import Database
 from Document import Document
+from DocumentAttachment import DocumentAttachment
 from Uuid import Uuid
 # from http.Stream import Stream
