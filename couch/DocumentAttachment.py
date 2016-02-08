a7212c3d535f435dce180d1549af086ddc73ed1d
Kerem Güneş <qeremy@gmail.com>
2016-02-08 02:46:44 +0200

    add properties & constructor.

--- couch/DocumentAttachment.py
+++ couch/DocumentAttachment.py
@@ -1,5 +1,14 @@
 import couch
 import couch.util.Util as util
 
-class DocumentAttachment():
-   pass
+class DocumentAttachment(object):
+   document = None
+   file, fileName = None, None
+   data, dataLength = None, 0
+   contentType = None
+   digest = None
+
+   def __init__(self, document = None, file = None, fileName = None):
+      pass
+
+
